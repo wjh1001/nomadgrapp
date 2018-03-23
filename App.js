84bcf54014f2d88dc20a622de5411960b9775628
Andy Woo <cheese.wo@gmail.com>
2018-03-23 19:52:02 +0900

    err

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { AppLoading, Asset, Font } form "expo"
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  }
+  render(){
+    return{
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text> Open up App.js to start working on your appp!</Text>
       </View>
-    );
+    }
   }
 }
 
