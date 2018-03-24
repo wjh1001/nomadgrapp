985e2bd99380a379ed2403b4159cbe133d3ad196
Andy Woo <cheese.wo@gmail.com>
2018-03-25 00:55:02 +0900

    #4-7 LogInScreen part 1

--- components/AppContainer/presenter.js
+++ components/AppContainer/presenter.js
@@ -1,34 +1,28 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { View, Text, StatusBar, StyleSheet } from "react-native";
-import LoggedOutNavigation from "../../navigation/LoggedOutNavigation"
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
+import LoggedOutNavigation from '../../navigation/LoggedOutNavigation';
 
 class AppContainer extends Component {
-  static propTypes = {
-    isLoggedIn: PropTypes.bool.isRequired
-  };
-  render() {
-    const { isLoggedIn } = this.props;
-    return (
-      <View style={styles.container}>
-        <StatusBar hidden={true} />
-        {isLoggedIn ? (
-          <Text>You are logged in</Text>
-        ) : (
-          <LoggedOutNavigation />
-        )}
-      </View>
-    );
-  }
+	static propTypes = {
+		isLoggedIn: PropTypes.bool.isRequired
+	};
+	render() {
+		const { isLoggedIn } = this.props;
+		return (
+			<View style={styles.container}>
+				<StatusBar hidden={false} />
+				{isLoggedIn ? <Text>You are logged in</Text> : <LoggedOutNavigation />}
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff'
+	}
 });
 
 export default AppContainer;
