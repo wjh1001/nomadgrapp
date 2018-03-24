195928051a07d6b2b3732bf9073c6905c613b4d6
Andy Woo <cheese.wo@gmail.com>
2018-03-24 23:36:34 +0900

    error: LoggedOutNavigation-can't find variable: Components

--- components/AppContainer/presenter.js
+++ components/AppContainer/presenter.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { View, Text, StatusBar, StyleSheet } from "react-native";
+import LoggedOutNavigation from "../../navigation/LoggedOutNavigation"
 
 class AppContainer extends Component {
   static propTypes = {
---
+++ screens/LogInScreen/container.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import LogInScreen from "./presenter";
+
+class Container extends Components {
+    render() {
+        return <LogInScreen />;
+
+    }
+}
+
+export default Container;
