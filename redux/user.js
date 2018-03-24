158822c1c3d05f820c51cb6ae4a787974e39cb3a
Andy Woo <cheese.wo@gmail.com>
2018-03-24 11:05:45 +0900

    #4-2 Persisting the Redux Store part 1

---
+++ redux/user.js
@@ -0,0 +1,28 @@
+// Imports
+
+// Actions
+
+// Action Creators
+
+// API Actions
+
+// Initial state
+
+const initialState = {
+    isLoggedIn: false
+}
+
+// Reducer
+
+function reducer(state = initialState, action){
+    switch (action.type){
+        default:
+            return state;
+    }
+}
+
+// Export
+
+// Default Reducer Export
+
+export default reducer
