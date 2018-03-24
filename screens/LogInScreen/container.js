62a039e72ec06e0a58eb3519bab6f679fab2cbd0
Andy Woo <cheese.wo@gmail.com>
2018-03-24 23:50:48 +0900

    nav option change from "LoggedOutNavigation" to "/LoginScreen/container.js"

--- screens/LogInScreen/container.js
+++ screens/LogInScreen/container.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import LogInScreen from "./presenter";
 
 class Container extends Component {
+    static navigationOptions = ({navigation}) => ({
+        title: "Log in"
+    });
     render() {
         return <LogInScreen />;
 
