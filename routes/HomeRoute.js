70d67f42ea59c949cff5265e5910544475953277
Andy Woo <cheese.wo@gmail.com>
2018-07-01 20:52:48 +0900

    mange Header on Route(better way)

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { Image } from "react-native";
+import NavButton from "../components/NavButton";
 import {
     StackNavigator
 } from "react-navigation";
@@ -8,10 +11,25 @@ import sharedRoutes, {
 
 const HomeRoute = StackNavigator({
     Home: {
-        screen: FeedScreen
+        screen: FeedScreen,
         // navigationOptions: {
         //     title: "Nomadgram"
         // }
+        navigationOptions: ({ navigation }) =>  ({
+            headerTitle:(
+            <Image 
+                source={require("../assets/images/logo.png")}
+                style={{ height: 35}}
+                resizeMode={"contain"}
+            />
+            ),
+            headerLeft:(
+                <NavButton 
+                    iconName={"ios-camera-outline"}
+                    onPress={() => navigation.navigate("TakePhoto")}
+                />
+            )
+        })
 
     },
     ...sharedRoutes
