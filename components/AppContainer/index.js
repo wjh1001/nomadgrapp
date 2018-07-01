cfee43a0564fe6b4c9de4b52bc05af91c2ec6868
Andy Woo <cheese.wo@gmail.com>
2018-07-02 00:21:14 +0900

    #4-24 Creating the getOwnProfile and getNotifications Functions

--- components/AppContainer/index.js
+++ components/AppContainer/index.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import AppContainer from "./presenter";
 import { actionCreators as photoAcitons } from "../../redux/modules/photo";
+import { actionCreators as userActions } from "../../redux/modules/user";
 
 const mapStateToProps = (state, ownProps) => {
     const { user } = state;
@@ -15,8 +16,8 @@ const mapStateToProps = (state, ownProps) => {
 const const mapDispatchToProps = (dispatch, ownProps) => {
     return {
         initApp: () => {
-            dispatch(getFeed()); //getFeed
-            //getSearch
+            dispatch(phtoActions.getFeed()); //getFeed
+            dispatch(phtoActions.getSearch());  //getSearch
             //getNotifications
             //getProfile
         }
--- redux/modules/photo.js
+++ redux/modules/photo.js
@@ -4,6 +4,10 @@ import { ActionCreator as userActions } from "./user"
 
 // actions
 
+
+const SET_FEED = "SET_FEED";
+const SET_SEARCH = "SET_SEARCH";
+
 // action Creators
 
 function setFeed(feed){
@@ -13,9 +17,16 @@ function setFeed(feed){
     };
 }
 
+function setSearch(search){
+    return {
+        type: SET_SEARCH,
+        search
+    };
+}
+
 
 // API actions
- function getFeed(){
+function getFeed(){
      return (dispatch, getState) => {
          const { user: { token } } = getState();
          fetch(`${API_URL}/images/`, {
@@ -33,7 +44,29 @@ function setFeed(feed){
          })
          .then(json => dispatch(setFeed))
      }
- }
+}
+
+function getSearch(){
+    return (dispatch, getState) => {
+        const { user: { token } } = getState();
+        fetch(`${API_URL}/images/search/`, {
+            headers: {
+                Authorizations: `JWT ${token}`
+            }
+        })
+        .then(response => {
+            if(response.status === 401){
+                dispatch(userActions.logOut());
+            } else {
+                return response.json();
+            }
+
+        })
+        .then(json => dispatch(setSearch(json)))
+    }
+}
+
+
 
 
 // initial State
@@ -45,7 +78,9 @@ const initialState = {}
 const reducer(state = initialState, action ){
     switch (action.type){
         case SET_FEED:
-        return applySetFeed(state, action);
+            return applySetFeed(state, action);
+        case SET_SEARCH:
+            return applySetSearch(state, action);
 
         default:
         return state;
@@ -55,7 +90,7 @@ const reducer(state = initialState, action ){
 // reducer functions
 
 function applySetFeed(state, action){
-    const { feed } = action
+    const { feed } = action;
     return {
         ...state,
         feed
@@ -63,10 +98,19 @@ function applySetFeed(state, action){
 }
 
 
+function applySetSearch(state, action ){
+    const { search } = action;
+    return {
+        ...state,
+        search
+    }
+}
+
 // exports
 
 const actionCreators = {
-    getFeed
+    getFeed,
+    getSearch
 };
 
 export { actionCreators };
